18165a64d06855835bed0f4f6b11cf286b940f70
NikhilKatigar <nikhilkatigar76@example.com>
2026-02-05 15:59:05 +0530

    Fix API baseURL to include /api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://college-scraper-backend.onrender.com",
+  baseURL: "https://college-scraper-backend.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
